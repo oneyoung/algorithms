4c6722de58ba4cfae4139bb8cc5882dfa12e3d8e
Wayne <guowangyang@gmail.com>
2013-08-12 15:27:02 +0800

    + sort: test running profile

--- sort/tests.py
+++ sort/tests.py
@@ -1,4 +1,6 @@
 import unittest
+import time
+import random
 from copy import deepcopy
 import sort
 
@@ -10,10 +12,32 @@ class TestSort(unittest.TestCase):
                       'quick_sort',
                       'quick_sort2', ]
 
+    def get_func(self, func):
+        return sort.__dict__[func]
+
     def test_sanity(self):
         ilist = [2, 4, 1, 9, 7, 5, 3]
         olist = [1, 2, 3, 4, 5, 7, 9]
 
         for func in self.funcs:
-            result = sort.__dict__[func].__call__(deepcopy(ilist))
+            result = self.get_func(func)(deepcopy(ilist))
             self.assertListEqual(result, olist)
+
+    def test_runingtime(self):
+        def record_time(func, array):
+            start = time.time()
+            func(array)
+            end = time.time()
+
+            return end - start
+
+        # we might meet max recursion limit, set to a bigger value
+        import sys
+        sys.setrecursionlimit(99999)
+
+        array0 = range(5000)
+        random.shuffle(array0)
+
+        for func in self.funcs:
+            array = deepcopy(array0)
+            print 'method %s spend %s' % (func, record_time(self.get_func(func), array))
